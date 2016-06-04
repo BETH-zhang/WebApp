9991421451f7ff1c073d7125a0887ea9c82768b6
beth <zhangbaige@innobuddy.com>
2016-06-04 17:10:04 +0800

    完成雷达图组件

--- js/H5ComponentRadar.js
+++ js/H5ComponentRadar.js
@@ -59,6 +59,30 @@ var H5ComponentRadar = function(name, cfg){
 		var y = r + Math.cos(rad) * r;
 		ctx.moveTo(r, r);
 		ctx.lineTo(x, y);
+
+		// 输出项目文字
+		var text = $('<div class="text">');
+		text.text(cfg.data[i][0]);
+
+		text.css('transition', 'all .5s ' + i*.1 + 's');
+
+		if(x>w/2){
+			text.css('left', x/2+5);
+		}else{
+			text.css('right', (w-x)/2+5);
+		}
+
+		if(y>h/2){
+			text.css('top', y/2+5);
+		}else{
+			text.css('bottom', (h-y)/2+5);
+		}
+
+		if(cfg.data[i][2]){
+			text.css('color', cfg.data[i][2]).css('opacity', 0);
+		}
+
+		component.append(text);
 	}
 	ctx.strokeStyle = '#e0e0e0';
 	ctx.stroke();
@@ -74,6 +98,13 @@ var H5ComponentRadar = function(name, cfg){
 	ctx.strokeStyle = '#f00';
 
 	var draw = function(per){
+		if(per <= 1){
+			component.find('.text').css('opacity', 0);
+		}
+		if(per >= 1){
+			component.find('.text').css('opacity', 1);
+		}
+
 		ctx.clearRect(0, 0, w, h);
 		// 输出数据的折线
 		for(var i=0;i<step;i++){
@@ -122,7 +153,7 @@ var H5ComponentRadar = function(name, cfg){
 		for(i=0;i<100;i++){
 			setTimeout(function(){
 				s-=.01;
-				// draw(s);
+				draw(s);
 			}, i*10+500);
 		}
 
