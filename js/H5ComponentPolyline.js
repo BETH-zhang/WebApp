4e6c1acea63dd7c00692957e0c7133c0ee055e63
beth <zhangbaige@innobuddy.com>
2016-06-04 14:34:53 +0800

    add canvas

--- js/H5ComponentPolyline.js
+++ js/H5ComponentPolyline.js
@@ -1,2 +1,20 @@
 /* 柱图组件对象 */
 
+/* 柱图组件对象 */
+
+var H5ComponentPolyline = function(name, cfg){
+	var component = new H5ComponentBase(name, cfg);
+
+	// 绘制网格线
+	var w = cfg.width;
+	var h = cfg.height;
+	
+	// 加入一个画布（网格线背景）
+	var cns = document.createElement('canvas');
+	var ctx = cns.getContext('2d');
+	cns.width = ctx.width = w;
+	cns.height = ctx.height = h;
+	component.append(cns);
+
+	return component;
+}
