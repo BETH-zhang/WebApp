425d43b7a7e573df220f582fb3b55049993d8f68
beth <zhangbaige@innobuddy.com>
2016-06-07 15:20:09 +0800

    修改部分样式

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -1,61 +1,73 @@
 /* 基本图文组件对象 */
 
 var H5ComponentBase = function(name, cfg){
-	var cfg = cfg || {};
-	var id = ('h5_c_' + Math.random()).replace('.', '_');
+  var cfg = cfg || {};
+  var id = ('h5_c_' + Math.random()).replace('.', '_');
 
-	var cls = ' h5_component_' + cfg.type; //把当前的组件类型添加到样式中进行标记
-	var component = $('<div class="h5_component '+cls+' h5_component_name_' + name + '" id="'+id+'">');
-	
-	cfg.text && component.text(cfg.text);
-	cfg.width && component.width(cfg.width/2);
-	cfg.height && component.height(cfg.height/2);
+  var cls = ' h5_component_' + cfg.type; //把当前的组件类型添加到样式中进行标记
+  var component = $('<div class="h5_component '+cls+' h5_component_name_' + name + '" id="'+id+'">');
+  
+  cfg.text && component.text(cfg.text);
+  cfg.width && component.width(cfg.width/2);
+  cfg.height && component.height(cfg.height/2);
 
-	cfg.css && component.css(cfg.css);
-	cfg.bg && component.css('backgroundImage', 'url('+cfg.bg+')');
+  cfg.css && component.css(cfg.css);
+  cfg.bg && component.css('backgroundImage', 'url('+cfg.bg+')');
 
-	if(cfg.center === true){
-		component.css({
-			marginLeft: (cfg.width/4 * -1) + 'px',
-			left: '50%'
-		});
-	}
+  if(cfg.center === true){
+    component.css({
+      marginLeft: (cfg.width/4 * -1) + 'px',
+      left: '50%'
+    });
+  }
 
-	// ...很多自定义的参数
-	if(typeof cfg.onclick === 'function'){
-		component.on('click', cfg.onclick);
-	}
+  // ...很多自定义的参数
+  if(typeof cfg.onclick === 'function'){
+    component.on('click', cfg.onclick);
+  }
+  if(typeof cfg.resetTextFun === 'function'){
+    var getDays = cfg.resetTextFun();
 
-	//任务一：支持 relativeTo参数（CSS translate 实现方法）
-	//任务二：获取 relateveTo元素的位置，应该用offsetLeft、offsetTop
-	//任务二：考虑 cfg.center 对relativeTo参数的影响，并且在有，没有这两种情况下都要支持
-	//不知道怎么用
-	if(cfg.relateveTo){
-		var parent = $('body').find('.h5_component_name_'+cfg.relateveTo);
-		var position = {
-			left: $(parent)[0].offsetLeft,
-			top: $(parent)[0].offsetTop,
-		};
-		if(cfg.center === true){
-			position.left = 0;
-		}
-		component.css('transform', 'translate('+position.left+'px,'+position.top+'px)');
-	}
-	
+    // var val = 0;
+    // for(var i=1;i<= getDays; i++){
+   //        setTimeout(function(){
+   //            val += 1;
+    //    console.log('val', val);
+   //        }, i);
+   //    }
+    getDays && component.text(getDays);
+  }
+
+  //任务一：支持 relativeTo参数（CSS translate 实现方法）
+  //任务二：获取 relateveTo元素的位置，应该用offsetLeft、offsetTop
+  //任务二：考虑 cfg.center 对relativeTo参数的影响，并且在有，没有这两种情况下都要支持
+  //不知道怎么用
+  if(cfg.relateveTo){
+    var parent = $('body').find('.h5_component_name_'+cfg.relateveTo);
+    var position = {
+      left: $(parent)[0].offsetLeft,
+      top: $(parent)[0].offsetTop,
+    };
+    if(cfg.center === true){
+      position.left = 0;
+    }
+    component.css('transform', 'translate('+position.left+'px,'+position.top+'px)');
+  }
+  
     component.on('onLoad', function(){
-    	setTimeout(function(){
-	        component.addClass(cls + '_load').removeClass(cls + '_leave');
-	        cfg.animateIn && component.animate(cfg.animateIn)
-    	}, cfg.delay || 0);
+      setTimeout(function(){
+          component.addClass(cls + '_load').removeClass(cls + '_leave');
+          cfg.animateIn && component.animate(cfg.animateIn)
+      }, cfg.delay || 0);
         return false;
     });
-    component.on('onLeave', function(){    	
-    	setTimeout(function(){
-	        component.addClass(cls + '_leave').removeClass(cls + '_load');
-	        cfg.animateOut && component.animate(cfg.animateOut)
-    	}, cfg.delay || 0);
+    component.on('onLeave', function(){     
+      setTimeout(function(){
+          component.addClass(cls + '_leave').removeClass(cls + '_load');
+          cfg.animateOut && component.animate(cfg.animateOut)
+      }, cfg.delay || 0);
         return false;
     });
     
-	return component;
+  return component;
 }
